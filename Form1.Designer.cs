e5208dcfa1e61e9ecb33874319c82a364a0c51b6
i.kokkalis <i.kokkalis@HW-KOKKALIS>
2022-04-26 11:25:15 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿
+namespace TrayFolderMenu
+{
+    partial class frmOptions
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmOptions));
+            this.icoNotify = new System.Windows.Forms.NotifyIcon(this.components);
+            this.mnuRightClick = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.lstFolders = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // icoNotify
+            // 
+            this.icoNotify.ContextMenuStrip = this.mnuRightClick;
+            this.icoNotify.Icon = ((System.Drawing.Icon)(resources.GetObject("icoNotify.Icon")));
+            this.icoNotify.Visible = true;
+            this.icoNotify.DoubleClick += new System.EventHandler(this.icoNotify_DoubleClick);
+            // 
+            // mnuRightClick
+            // 
+            this.mnuRightClick.Name = "mnuLeftClickMenu";
+            this.mnuRightClick.Size = new System.Drawing.Size(61, 4);
+            // 
+            // lstFolders
+            // 
+            this.lstFolders.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstFolders.FormattingEnabled = true;
+            this.lstFolders.ItemHeight = 15;
+            this.lstFolders.Location = new System.Drawing.Point(12, 12);
+            this.lstFolders.Name = "lstFolders";
+            this.lstFolders.Size = new System.Drawing.Size(470, 214);
+            this.lstFolders.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAdd.Location = new System.Drawing.Point(488, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(32, 32);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRemove.Location = new System.Drawing.Point(488, 50);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(32, 32);
+            this.btnRemove.TabIndex = 4;
+            this.btnRemove.Text = "-";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.Location = new System.Drawing.Point(450, 244);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(32, 32);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "X";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOK.Location = new System.Drawing.Point(488, 244);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(32, 32);
+            this.btnOK.TabIndex = 6;
+            this.btnOK.Text = "✓";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.LoadFolderMenus);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnExit.Location = new System.Drawing.Point(12, 244);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(122, 32);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit Application";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmOptions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 288);
+            this.ControlBox = false;
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lstFolders);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "frmOptions";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tray Folder Menu Options";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.frmOptions_Load);
+            this.Shown += new System.EventHandler(this.frmOptions_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon icoNotify;
+        private System.Windows.Forms.ContextMenuStrip mnuRightClick;
+        private System.Windows.Forms.ListBox lstFolders;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOK;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,300 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrayFolderMenu
+{
+    public partial class frmOptions : Form
+    {
+        public ConcurrentDictionary<string, Image> CachedIcons = new ConcurrentDictionary<string, Image>();
+        public List<string> linkExtensions = new List<string>() { ".lnk", ".url" };
+
+        public frmOptions()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void frmOptions_Load(object sender, EventArgs e)
+        {
+            lstFolders.Items.Clear();
+            foreach (var folder in Properties.Settings.Default.Folders)
+            {
+                lstFolders.Items.Add(folder);
+            }
+            mnuRightClick.Items.Clear();
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void icoNotify_DoubleClick(object sender, EventArgs e)
+        {
+            if (!backgroundWorker1.IsBusy)
+            {
+                this.Show();
+                this.BringToFront();
+            }
+            else
+            {
+                new ToastContentBuilder().AddText("Loading...").AddText("Please wait").Show();
+            }
+        }
+
+        #region Left Click
+
+        private void LoadFolderMenus(object sender, DoWorkEventArgs e)
+        {
+            var folders = new List<string>();
+            foreach (var folder in Properties.Settings.Default.Folders)
+            {
+                folders.Add(folder);
+            }
+            folders.Sort();
+
+            foreach (var folder in folders)
+            {
+                if (System.IO.Directory.Exists(folder))
+                {
+                    var menu = MakeFolderMenuItem(folder);
+                    MakeTree(folder, menu);
+                    mnuRightClick.Items.Add(menu);
+                }
+            }
+        }
+
+        private void MakeTree(string folder, ToolStripMenuItem parentMenu)
+        {
+            if (System.IO.Directory.Exists(folder))
+            {
+                var subFolders = System.IO.Directory.GetDirectories(folder);
+                var files = System.IO.Directory.GetFiles(folder);
+                foreach (var subFolder in subFolders)
+                {
+                    var menu = MakeFolderMenuItem(subFolder);
+                    MakeTree(subFolder, menu);
+                    parentMenu.DropDownItems.Add(menu);
+                }
+
+                foreach (var file in files)
+                {
+                    var menu = MakeFileMenuItem(file);
+                    parentMenu.DropDownItems.Add(menu);
+                }
+
+            }
+        }
+
+        private ToolStripMenuItem MakeFolderMenuItem(string folder)
+        {
+            var menu = new ToolStripMenuItem();
+            menu.Text = System.IO.Path.GetFileName(folder);
+            menu.Tag = folder;
+            menu.DoubleClickEnabled = true;
+            menu.DoubleClick += new EventHandler(this.folderMenu_DoubleClick);
+            menu.MouseDown += new MouseEventHandler(this.folderMenu_Click);
+            menu.Image = icoNotify.Icon.ToBitmap();
+            return menu;
+        }
+
+        private ToolStripMenuItem MakeFileMenuItem(string file)
+        {
+            var menu = new ToolStripMenuItem();
+            var ext = System.IO.Path.GetExtension(file);
+            if (linkExtensions.Contains(ext))
+                menu.Text = System.IO.Path.GetFileNameWithoutExtension(file);
+            else
+                menu.Text = System.IO.Path.GetFileName(file);
+            menu.Tag = file;
+            menu.MouseDown += new MouseEventHandler(this.fileMenu_Click);
+            menu.Image = getIconFromFilename(file);
+            return menu;
+        }
+
+        private void folderMenu_DoubleClick(object sender, EventArgs e)
+        {
+            var tag = (string)((ToolStripMenuItem)sender).Tag;
+            if (!System.IO.Directory.Exists(tag))
+            {
+                new ToastContentBuilder().AddText($"Directory {tag} does not exist.").AddText("Please reload").Show();
+                return;
+            }
+
+            ProcessStartInfo psi = new ProcessStartInfo(tag);
+            psi.UseShellExecute = true;
+            Process.Start(psi);
+        }
+
+        private void folderMenu_Click(object sender, MouseEventArgs e)
+        {
+            var menuItem = (ToolStripMenuItem)sender;
+            var tag = (string)(menuItem.Tag);
+            if (!System.IO.Directory.Exists(tag))
+            {
+                new ToastContentBuilder().AddText($"Directory {tag} does not exist.").AddText("Please reload").Show();
+                return;
+            }
+
+            if (e.Button == MouseButtons.Right)
+            {
+                ShellContextMenu ctxMnu = new ShellContextMenu();
+                DirectoryInfo[] arrFI = new DirectoryInfo[1];
+                arrFI[0] = new DirectoryInfo(tag);
+                ToolStrip parent = menuItem.GetCurrentParent();
+                int y = 0;
+                foreach (ToolStripItem item in parent.Items)
+                {
+                    if (item == menuItem)
+                        break;
+                    if (item.Visible == true)
+                        y += item.Height;
+                }
+                var point = parent.PointToScreen(new Point(-4, y));
+                ctxMnu.ShowContextMenu(arrFI, point);
+            }
+        }
+
+
+        private void fileMenu_Click(object sender, MouseEventArgs e)
+        {
+            var menuItem = (ToolStripMenuItem)sender;
+            var tag = (string)(menuItem.Tag);
+            if (!System.IO.File.Exists(tag))
+            {
+                new ToastContentBuilder().AddText($"File {tag} does not exist.").AddText("Please reload").Show();
+                return;
+            }
+            if (e.Button == MouseButtons.Left)
+            {
+                ProcessStartInfo psi = new ProcessStartInfo(tag);
+                psi.UseShellExecute = true;
+                Process.Start(psi);
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                ShellContextMenu ctxMnu = new ShellContextMenu();
+                FileInfo[] arrFI = new FileInfo[1];
+                arrFI[0] = new FileInfo(tag);
+                ToolStrip parent = menuItem.GetCurrentParent();
+                int y = 0;
+                foreach (ToolStripItem item in parent.Items)
+                {
+                    if (item == menuItem)
+                        break;
+                    if (item.Visible == true)
+                        y += item.Height;
+                }
+                var point = parent.PointToScreen(new Point(-4, y));
+                ctxMnu.ShowContextMenu(arrFI, point);
+            }
+        }
+
+        #endregion
+
+        #region Right Click
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var fldWin = new FolderBrowserDialog();
+            if (fldWin.ShowDialog() == DialogResult.OK)
+            {
+                if (!lstFolders.Items.Contains(fldWin.SelectedPath))
+                {
+                    lstFolders.Items.Add(fldWin.SelectedPath);
+                }
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (lstFolders.SelectedItems.Count == 1)
+            {
+                lstFolders.Items.Remove(lstFolders.SelectedItems[0]);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            var folders = new System.Collections.Specialized.StringCollection();
+            foreach (var folder in lstFolders.Items)
+            {
+                folders.Add((string)folder);
+            }
+            Properties.Settings.Default.Folders = folders;
+            Properties.Settings.Default.Save();
+            this.Hide();
+            if (!backgroundWorker1.IsBusy)
+            {
+                mnuRightClick.Items.Clear();
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void frmOptions_Shown(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        #endregion
+
+
+        [DllImport("shell32.dll", CharSet = CharSet.Ansi)]
+        public static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes, ref SHFILEINFO psfi, uint cbSizeFileInfo, uint uFlags);
+
+        [DllImport("user32")]
+        public static extern int DestroyIcon(IntPtr hIcon);
+
+        public const uint SHGFI_ICON = 0x000000100;
+        public const uint SHGFI_SMALLICON = 0x000000001;
+        public const uint SHGFI_USEFILEATTRIBUTES = 0x000000010;
+
+        public Image getIconFromFilename(string fName)
+        {
+            if (System.IO.Path.HasExtension(fName) == false)
+                return null;
+
+            var ext = System.IO.Path.GetExtension(fName);
+            if (!linkExtensions.Contains(ext) && CachedIcons.ContainsKey(ext))
+                return CachedIcons[ext];
+
+            SHFILEINFO shinfo = new SHFILEINFO();
+            SHGetFileInfo(fName, 0, ref shinfo, (uint)Marshal.SizeOf(shinfo), SHGFI_USEFILEATTRIBUTES | SHGFI_ICON | SHGFI_SMALLICON);
+            Icon myIcon = (System.Drawing.Icon)(System.Drawing.Icon.FromHandle(shinfo.hIcon).Clone());
+            DestroyIcon(shinfo.hIcon);
+            var result = myIcon.ToBitmap();
+            CachedIcons.TryAdd(ext, result);
+            return result;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SHFILEINFO
+        {
+            public IntPtr hIcon;
+            public int iIcon;
+            public uint dwAttributes;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string szDisplayName;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 80)]
+            public string szTypeName;
+        };
+    }
+}
